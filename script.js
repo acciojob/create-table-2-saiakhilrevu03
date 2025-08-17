9ab334795ee84b95e073f03bc953315176f5f5c5
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-08-17 16:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 function createTable() {
     //Write your code here
+	let rn=prompt("number of rows");
+	let cn = prompt("number of columns");
+
+	rn = parseInt(rn);
+	cn = parseInt(cn);
+
+	if(isNaN(rn) || isNaN(cn)){
+		alert("please enter valid numbers!");
+		return;
+	}
+	if(rn<=0 || cn<=0){
+		alert("rows and columns must be greater than 0!");
+		return;
+	}
+
+	const table = document.getElementById("myTable");
+	table.innerHTML = "";
+
+	for(let i=0;i<rn;i++){
+		const row = document.createElement("tr");
+		for(let j=0;j<cn;j++){
+			const cell = document.createElement("td");
+			cell.textContent = `Row-${i}  Column-${j}`;
+			row.appendChild(cell);
+		}
+		table.appendChild(row);
+	}
+	
   
 }
+
